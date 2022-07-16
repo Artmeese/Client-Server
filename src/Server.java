a118ca64374c2b0a50d4c3c693103e2dd4f33be9
artsmmy <106704118+artsmmy@users.noreply.github.com>
2022-07-16 16:43:47 +0500

    Upd try-with-resources

--- src/Server.java
+++ src/Server.java
@@ -8,37 +8,24 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class Server {
-    
     private static void handle(Socket clientSocket) {
-        try {
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        try (PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))){
             System.out.println("New connection accepted");
-
             final String name = in.readLine();
             out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
-
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                clientSocket.close();
-            } catch (IOException e){
-                e.printStackTrace();
-            }
         }
     }
-    
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080);
         var executor = Executors.newFixedThreadPool(500);
 
-        try {
+        try (ServerSocket serverSocket = new ServerSocket(8080)){
             Socket clientSocket = serverSocket.accept();
             executor.submit(() -> handle(clientSocket));
         } catch (IOException e) {
             e.printStackTrace();
-            serverSocket.close();
         }
     }
 }
